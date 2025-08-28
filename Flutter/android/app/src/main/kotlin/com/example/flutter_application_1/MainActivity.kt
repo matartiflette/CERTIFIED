c19374c40cfe6a5c0fc9da6e79efb61335962c8c
Alex <child.garin@gmail.com>
2025-08-28 16:00:01 +0200

    debug - launch.json

---
+++ Flutter/android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.Flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- FlutterCertified/android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.CertifiedFlutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
